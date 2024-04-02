561c9c1b320bf4b0262deb76d42ecda13de21cb5
imsgowda <165708677+imsgowda@users.noreply.github.com>
2024-04-02 14:10:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,8 @@ ingredients_list = st.multiselect(
     , max_selections=5
 )
 
+st.write(ingredients_string)
+
 if ingredients_list:
       for fruit_chosen in ingredients_list:
             ingredients_string += fruit_chosen + ' '
@@ -38,10 +40,6 @@ if ingredients_list:
 
 search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
 st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-
-  
- 
-st.write(ingredients_string)
 
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
